bdcb22f9954f1636e5739c77da0715af52f7ad08
stella <stellaugochukwu100@gmail.com>
2022-09-19 10:00:52 -0700

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - generates random valid password
  * Return: 0 (on success)
  *
  */
-
 int main(void)
 {
 	int pass, sum;
