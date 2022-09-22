83fa9deba5c3aa5c901219919a3b3d78fb5aa1df
stella <stellaugochukwu100@gmail.com>
2022-09-22 10:19:46 -0700

    my header file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -14,5 +15,6 @@ void print_number(int n);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 #endif /* MAIN_H */
