1779ddbc5dccee7d15f2c52f81f924cb8f66365f
stella <stellaugochukwu100@gmail.com>
2022-10-03 16:01:46 -0700

    task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,10 +10,10 @@
 
 void free_grid(int **grid, int height)
 {
-	if (grid != NULL && height != 0)
+	if (len != NULL && height != 0)
 	{
 		for (; height >= 0; height--)
-			free(grid[height]);
-		free(grid);
+			free(len[height]);
+		free(len);
 	}
 }
