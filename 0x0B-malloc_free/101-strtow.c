b7effbea543d7c9b750df798a44d55e2eb659026
stella <stellaugochukwu100@gmail.com>
2022-10-03 14:12:52 -0700

    advanced task 2

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,138 +1,108 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
-
-int count_words(char *str);
-int *count_char(char *strlen, int c);
-char **split_string(char **arrystn, char *str);
-
 
 /**
-* strtow - a function that splits a string into words
-* @str: a ponter to the string to be operated on
-* Return: returns a pointer to a pointer to a string
-*/
+ * countWords - counts the numbers o words in the string
+ *
+ * @str: source string
+ *
+ * Return: the number of words.
+ */
+
+int countWords(char *str)
+{
+	int words;
+
+	words = 0;
+
+	while (*str)
+	{
+		if (*str != ' ')
+		{
+			if (*(str + 1) == ' ' || *(str + 1) == '\0')
+				words++;
+			str++;
+		}
+		else
+			str++;
+	}
+	return (words);
+}
+
+/**
+ * countChars - counts the numbers o characters in a string
+ *
+ * @str: source string
+ *
+ * Return: the number of words.
+ */
+
+int countChars(char *str)
+{
+	int chars;
+
+	chars = 0;
+	while (*str)
+	{
+		if (*str != ' ')
+		{
+			if (*(str + 1) != ' ' || *(str + 1) != '\0')
+				chars++;
+			str++;
+		}
+		else
+			break;
+	}
+	return (chars);
+}
+
+/**
+ * strtow - separates the character to an string of words
+ *
+ * @str: source string
+ *
+ * Return: the pointer to the new array of words.
+ */
+
 char **strtow(char *str)
 {
-	int i;
-	int c = 0;
-	int *strlen;
-	char **arrystn;
+	int numWords, numChars, i, j;
+	char **words;
 
-	if (str == NULL)
+	i = 0;
+	if (str == NULL || *str == '\0')
 		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
-		c++;
-	if (c == 0)
+	numWords = countWords(str);
+	if (numWords == 0)
 		return (NULL);
-
-	c = count_words(str);
-	strlen = count_char(str, c);
-
-	arrystn = (char **) malloc(c * sizeof(char *));
-	if (arrystn == NULL)
-		return (NULL);
-
-	for (i = 0; i < c; i++)
+	words = (char **) malloc((numWords + 1) * sizeof(char *));
+	if (words == NULL)
 	{
-		arrystn[i] = malloc((strlen[i] + 1) * sizeof(char));
-		if (arrystn[i] == NULL)
-			return (NULL);
+		free(words);
+		return (NULL);
 	}
-
-	arrystn = split_string(arrystn, str);
-	return (arrystn);
-}
-
-/**
-* count_words - counts the number of words in str, assuming every word is
-* seperated by spaces
-* @str: a pointer to the string to be counted
-* Return: returns an int
-*/
-int count_words(char *str)
-{
-	int i;
-	int c = 0;
-
-	for (i = 0; str[i] != 0; i++)
+	while (i < numWords)
 	{
-		if (str[i] != ' ' && str[i - 1] == ' ')
+		if (*str == ' ')
+			str++;
+		else
 		{
-			c++;
-		}
-	}
-	return (c);
-}
-
-/**
-* count_char - counts the number of characters in each word and stores it
-* in an array of integers
-* @str: the string with the words to be counted
-* @c: the number of words in str
-* Return: returns a pointer to the array of integers containing
-* the size of each word
-*/
-int *count_char(char *str, int c)
-{
-	int i = 0, j = 0, s = 0, k = 0;
-
-	int *strlen = malloc(c * sizeof(int));
-
-	if (strlen == NULL)
-		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		s = 0;
-		if (str[i] != ' ' && str[i - 1] == ' ')
-		{
-			for (j = i; str[j] != '\0'; j++)
+			numChars = countChars(str);
+			*(words + i) = (char *) malloc((numChars + 1) * sizeof(char));
+			if (*(words + i) == NULL)
 			{
-				if (str[j]  == ' ')
-				{
-					strlen[k] = s;
-					k++;
-					break;
-				}
-				s++;
+				for (j = 0; j < i; j++)
+					free(*(words + j));
+				free(words);
+				return (NULL);
 			}
-		}
-	}
-	return (strlen);
-}
-
-/**
-* split_string - puts each word from the original string str into a new
-* an array of strings arrystn, each string in the array is null-terminated
-* @arrystn: the array of strings
-* @str: the original string
-* Return: returns arrystn as a pointer to a pointer
-*/
-char **split_string(char **arrystn, char *str)
-{
-	int i, j, k, s;
-
-	k = 0;
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		s = 0;
-		if (str[i] != ' ' && str[i - 1] == ' ')
-		{
-			for (j = i; str[j] != '\0'; j++)
+			for (j = 0; j < numChars; j++)
 			{
-				if (str[j]  == ' ')
-				{
-					arrystn[k][j] = '\0';
-					break;
-				}
-				arrystn[k][s] = str[j];
-				s++;
-			}
-			k++;
+				*(*(words + i) + j) = *str;
+				str++; }
+			*(*(words + i) + j) = '\0';
+			i++;
 		}
 	}
-	arrystn[k] = NULL;
-	return (arrystn);
+	*(words + numWords) = NULL;
+	return (words);
 }
