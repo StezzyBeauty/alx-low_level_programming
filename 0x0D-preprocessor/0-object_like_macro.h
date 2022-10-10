686f0e726737e643842e96f3191d2c0cf8813bc0
stella <stellaugochukwu100@gmail.com>
2022-10-10 06:47:02 -0700

    task 0

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * Header file that defines a macro named SIZE as an abbreviation for the token 1024
+ */
+
+#define SIZE 1024
+
+#endif
