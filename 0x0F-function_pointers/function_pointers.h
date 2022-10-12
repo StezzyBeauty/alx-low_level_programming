d9e923c0cb1cf7380f698bf4104c320abb40af0c
stella <stellaugochukwu100@gmail.com>
2022-10-12 04:21:07 -0700

    header file for function pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,11 @@
-#include <stdlib.h>
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
 
-int _putchar(char c);
+#include <stddef.h>
+
+int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif /* FUNCTION_POINTERS_H */
+#endif /* _FUNCTION_POINTERS_H_ */
