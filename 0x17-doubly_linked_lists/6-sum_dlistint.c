571ee3d7cb995c68e47ee81e935cd9807506bf32
stella <stellaugochukwu100@gmail.com>
2022-12-08 08:27:17 -0800

    task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - returns the sum of all the data (n)
+ * sum_dlistint - it returns the sum of all the data (n)
  * of a doubly linked list
  *
  * @head: head of the list
